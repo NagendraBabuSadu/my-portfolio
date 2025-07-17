7c876a84c3b0295aac31349a276e1da67bde7501
Bob Cheg <89573050+BobCheg@users.noreply.github.com>
2025-07-17 19:50:45 +0530

    updating

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth scrollbar-hide">
        <body
-      className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden overflow-y-scroll scrollbar-hidden`}
+      className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden overflow-y-scroll scrollbar-hidden mt-20`}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         <Navbar />
         {children}
       </div>
